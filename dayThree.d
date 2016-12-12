ed1a2d5edd8978fa4d89e30e69d0ec7fe58ee7c0
Jason Yegge <jyegge@backflipstudios.com>
2016-12-12 12:38:08 -0700

    day three part two

--- dayThree.d
+++ dayThree.d
@@ -6,11 +6,25 @@ import std.conv;
 
 void main()
 {
-	auto triangles = stdin.byLineCopy							//input, by line
-		.map!(entry=>entry.splitter.map!(to!int).array)			// "1 2 3" => [1,2,3]
-		.filter!(tri=>tri
-			.permutations										//wasteful as this generates more permutations than we need
-			.all!(perm=>perm.take(2).sum > perm.tail(1)[0]));	//take only those where every permutation produces the first two sides greater than the third.
+	auto input = stdin.byLineCopy										//input, by line
+					  .map!(entry=>entry.splitter.map!(to!int).array)	// "1 2 3" => [1,2,3]
+					  .array;
 
-	writeln("triangles: " ~ triangles.count.to!string);
+	auto rowTriangles = input
+						.filter!validTriangle;
+
+	writeln("row triangles: " ~ rowTriangles.count.to!string);
+
+		auto columnTriangles = input.array
+			.transposed.joiner.array.chunks(3)
+			.filter!validTriangle;
+	
+	writeln("column triangles: " ~ columnTriangles.count.to!string);
+	
+}
+
+bool validTriangle(Range)(Range r)
+{
+	return r.permutations									//wasteful as this generates more permutations than we need
+			.all!(perm=>perm.take(2).sum > perm.tail(1)[0]);//valid only if every permutation's first two sides are greater than the third
 }
