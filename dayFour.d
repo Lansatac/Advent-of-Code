e330b9f53f0ba3309721b5834503d7e08e7bdceb
Jason Yegge <jyegge@backflipstudios.com>
2016-12-13 09:51:52 -0700

    simplifying slightly

--- dayFour.d
+++ dayFour.d
@@ -11,7 +11,7 @@ void main()
 {
 	auto rooms = stdin.byLineCopy.array
 				.map!(entry=>entry.splitter("-"))
-				.map!(room=>tuple(room.array
+				.map!(room=>room.array
 									  .retro.drop(1)
 									  .joiner
 									  .array.stdsort
@@ -19,8 +19,10 @@ void main()
 									  .array.multiSort!("a[1] > b[1]", "a[0] < b[0]")
 									  .map!(t=>t[0])
 									  .take(5).array,
-								  room.array.back.array.only
-								      .map!(id=>tuple(id.take(3).array, id.drop(4).take(5).array)).front))
+					  room=>room.array.back
+					  			.array.only
+							    .map!(id=>id.take(3).array,
+							 		  id=>id.drop(4).take(5).array).front)
 				.filter!(room=>room[0] == room[1][1]);
 
 	writeln(rooms.map!(room=>room[1][0].to!int).sum);
