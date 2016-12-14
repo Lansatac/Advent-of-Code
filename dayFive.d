a604dd8b8b73845123acd4e3f580854a485d4ad1
Jason Yegge <jyegge@backflipstudios.com>
2016-12-14 09:41:13 -0700

    day five part two

--- dayFive.d
+++ dayFive.d
@@ -1,24 +1,52 @@
 import std.stdio;
 import std.algorithm;
-import std.typecons;
 import std.range;
 import std.conv;
-import std.math;
+import std.array;
+import std.string;
 import std.digest.md;
-
-alias stdsort = std.algorithm.sort;
 
 void main()
 {
     auto numbers = iota(0, int.max);
     auto doorName = stdin.byLineCopy.front;
 
-    auto code = numbers
+    auto hashes = numbers
                     .map!(index=>doorName ~ index.to!string)
                     .map!(code=>md5Of(code).toHexString!(LetterCase.lower).idup)
-                    .filter!(hash=>hash.startsWith("00000"))
-                    .take(8)
-                    .map!(hash=>hash.drop(5).front);
+                    .filter!(hash=>hash.startsWith("00000"));
+                    
+    char[8] codeOne;
+    int codeOneIndex;
+    char[8] codeTwo;
+    int codeTwoCount;
+    while(codeTwoCount < 8)
+    {
+        auto hash = hashes.front;
+        char sixth = hash[5];
+        if(codeOneIndex < codeOne.length)
+        {
+            codeOne[codeOneIndex++] = sixth;
+            drawResults(codeOne, codeTwo);
+        }
 
-    writeln(code);
+        auto sixthString = sixth.to!string;
+        if(sixthString.isNumeric)
+        {
+            int index = sixthString.to!int;
+            if(index < codeTwo.length && codeTwo[index] == char.init)
+            {
+                codeTwo[index] = hash[6];
+                ++codeTwoCount;
+                drawResults(codeOne, codeTwo);
+            }
+        }
+        hashes.popFront();
+    }
+}
+
+void drawResults(char[] one, char[] two)
+{
+    writefln("1: %s", one.replace(char.init.to!string, "_"));
+    writefln("2: %s", two.replace(char.init.to!string, "_"));
 }
